8b1fe7388f28862a0ec6ac0ec38cb0fed8aee6b9
jibonghoshbd <jibonghosh1510@gmail.comm>
2021-10-06 12:38:55 +0600

    simple-firebase-authenticion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useState } from "react";
 import './App.css';
+import initilizeAuthentication from './Firebase/firebase-initilize';
+
+initilizeAuthentication()
+const googleProvider = new GoogleAuthProvider();
 
 function App() {
+  const [user, setUser] = useState({})
+  const auth = getAuth();
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        const { displayName, email, photoURL } = result.user;
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        };
+        setUser(loggedInUser);
+      })
+      .catch(error => {
+        console.log(error.message);
+      })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      <br />
+      {
+        user.email && <div>
+          <h2>Welcome {user.name}</h2>
+          <p>I know your email address: {user.email}</p>
+          <img src={user.photo} alt="" />
+        </div>
+      }
+
     </div>
   );
 }
---
+++ src/Firebase/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAFtzNk4w7OHgLEPbwDSzTwD4cMSP7XRA0",
+    authDomain: "simple-firebase-aouth.firebaseapp.com",
+    projectId: "simple-firebase-aouth",
+    storageBucket: "simple-firebase-aouth.appspot.com",
+    messagingSenderId: "814419924789",
+    appId: "1:814419924789:web:ed4cd98d323fd843c09e7c"
+};
+
+export default firebaseConfig;
